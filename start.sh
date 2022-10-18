d86fd42ac61952346c8fcf76f8bf902c1dc13aa5
yoauf <104299268+yoauf@users.noreply.github.com>
2022-10-17 21:11:20 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+PORT=8080
+UUID=378b26a4-4ea3-11ed-bdc3-0242ac120002
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
